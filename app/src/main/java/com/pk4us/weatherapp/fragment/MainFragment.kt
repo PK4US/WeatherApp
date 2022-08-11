4653cec6fea71e37001de28dae377bc2726afeb9
PK4U_S <Stepanchuk.ei@gmail.com>
2022-08-11 23:06:57 +0300

    Проверка на разрешение Fine location

--- app/src/main/java/com/pk4us/weatherapp/fragment/MainFragment.kt
+++ app/src/main/java/com/pk4us/weatherapp/fragment/MainFragment.kt
@@ -1,23 +1,43 @@
 package com.pk4us.weatherapp.fragment
 
+import android.Manifest
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import com.pk4us.weatherapp.R
 import com.pk4us.weatherapp.databinding.ActivityMainBinding
 import com.pk4us.weatherapp.databinding.FragmentMainBinding
 
 class MainFragment : Fragment() {
+    private lateinit var pLauncher: ActivityResultLauncher<String>
 private lateinit var binding: FragmentMainBinding
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         binding = FragmentMainBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        chekPermission()
+    }
+
+    private fun permissionListener(){
+        pLauncher = registerForActivityResult(ActivityResultContracts.RequestPermission()){
+            Toast.makeText(activity,"Permission is $it",Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun chekPermission(){
+        if (!isPermissionGranted(Manifest.permission.ACCESS_FINE_LOCATION)){
+            permissionListener()
+            pLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+        }
     }
 
     companion object {
