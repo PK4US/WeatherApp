4632ec1911ea7e3d01a342a27a2e2846a935440d
PK4U_S <Stepanchuk.ei@gmail.com>
2022-08-12 16:14:56 +0300

    Получение данных с сервера

--- app/src/main/java/com/pk4us/weatherapp/fragments/MainFragment.kt
+++ app/src/main/java/com/pk4us/weatherapp/fragments/MainFragment.kt
@@ -2,6 +2,7 @@ package com.pk4us.weatherapp.fragments
 
 import android.Manifest
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -11,9 +12,14 @@ import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.FragmentActivity
+import com.android.volley.Request
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 import com.google.android.material.tabs.TabLayoutMediator
 import com.pk4us.weatherapp.adapters.VpAdapter
 import com.pk4us.weatherapp.databinding.FragmentMainBinding
+
+const val API_KEY = "3442427ab7b944acb4892702221108"
 
 class MainFragment : Fragment() {
     private val fList = listOf(
@@ -37,6 +43,7 @@ class MainFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         chekPermission()
         init()
+        requestWeatherData("London")
     }
 
     private fun init() = with(binding){
@@ -58,6 +65,21 @@ class MainFragment : Fragment() {
             permissionListener()
             pLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
         }
+    }
+
+    private fun requestWeatherData(city:String){
+        val url = "https://api.weatherapi.com/v1/forecast.json?key=" +
+                API_KEY+
+                "&q=" +
+                city +
+                "&days=" +
+                "3" +
+                "&aqi=no&alerts=no"
+        val queue = Volley.newRequestQueue(context)
+        val request = StringRequest(Request.Method.GET,url,{
+                result -> Log.d("MyLog","result: $result")}, {
+                error -> Log.d("MyLog","error: $error")})
+        queue.add(request)
     }
 
     companion object {
