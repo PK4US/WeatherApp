cbc47f51ba356e58ebf86a9b49c1f8942c2f0390
PK4U_S <Stepanchuk.ei@gmail.com>
2022-08-12 14:30:02 +0300

    data class WeatherModel и разметка list_item

---
+++ app/src/main/java/com/pk4us/weatherapp/adapters/WeatherModel.kt
@@ -0,0 +1,12 @@
+package com.pk4us.weatherapp.adapters
+
+data class WeatherModel(
+    val city:String,
+    val time:String,
+    val condition:String,
+    val currentTemp:String,
+    val maxTemp:String,
+    val minTemp:String,
+    val imageURL:String,
+    val hours:String,
+)
